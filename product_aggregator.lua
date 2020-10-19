988244618c01955146c3c9e39a37c049ca003cbc
Michal Broniewicz <mbroniewicz@outlook.com>
2020-10-19 17:35:19 +0200

    Refactoring

--- product_aggregator.lua
+++ product_aggregator.lua
@@ -2,42 +2,43 @@ local product_aggregator = {
     products = {}
 }
 
+local function compare(_first, _second)
+    return _first.price < _second.price
+end
+
+local function sortProducts(self)
+    table.sort(self.products, compare)
+end
+
+local function removeIfExistsWithLowerPrice(self, _product)
+    for j, k in pairs(self.products) do
+        if k.name == _product.name and k.price <= _product.price then
+            table.remove(self.products, j)
+        end
+    end
+end
+
+local function isProductValid(_product)
+    for j, k in pairs(_product) do
+        if (j == 'name' and type(k) ~= 'string') or (j == 'price' and (type(k) ~= 'number' or k < 0)) then
+            return false
+        end
+    end
+    return true
+end
+
+local function validateProducts(self, _products)
+    for _, k in pairs(_products) do
+        if isProductValid(k) then
+            removeIfExistsWithLowerPrice(self, k)
+            table.insert(self.products, k)
+        end
+    end
+end
+
 function product_aggregator:new(_items)
-    local validateProducts = function (_products)
-        local isProductValid = function (_product)
-            for j, k in pairs(_product) do
-                if (j == 'name' and type(k) ~= 'string') or (j == 'price' and (type(k) ~= 'number' or k < 0)) then
-                    return false
-                end
-            end
-            return true
-        end
-
-        local removeIfExistsWithLowerPrice = function (_product)
-            for j, k in pairs(self.products) do
-                if k.name == _product.name and k.price <= _product.price then
-                    table.remove(self.products, j)
-                end
-            end
-        end
-
-        for _, k in pairs(_products) do
-            if isProductValid(k) then
-                removeIfExistsWithLowerPrice(k)
-                table.insert(self.products, k)
-            end
-        end
-    end
-
-    local sortProducts = function ()
-        local compare = function (_first, _second)
-            return _first.price < _second.price
-        end
-        table.sort(self.products, compare)
-    end
-
-    validateProducts(_items)
-    sortProducts()
+    validateProducts(self, _items)
+    sortProducts(self)
 end
 
 function product_aggregator:getProducts()
